51461aa82409013c9c216af014de31dd5a685440
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-05-27 02:53:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # -> lib/tic_tac_toe.rb
 
 def display_board(board)
-  puts " # | # | # "
+  puts "   |   |   "
   puts "-----------"
-  puts " # | # | # "
+  puts "   |   |   "
   puts "-----------"
-  puts " # | # | # "
+  puts "   |   |   "
 end
